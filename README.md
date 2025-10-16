d6d61ba61513979ed3d55f0398bd568485ae10ae
fxc7ss <fxc7ss@virigina.edu>
2025-10-16 04:36:35 -0400

    update

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 Various examples and short exercises on bridging Python and compiled code
   
+# PROJECT 2 
+Python baseline was 29s
+C Shared lib was 2.46s
+Python MC lib was 1.53s
+
+We are only measuing the wall time since that what timeit -r1 -n1 measures as well. 
+
+The speed up mostly occurs with the memory chunking (making sections of the monte carlo box) and the vectorized numpy arrays. 
+We are generating up to a certain amount of the grid to being with at once instead of processing each point. We are also now avoiding 
+using the sqrt since that would increase time.
--- mylib.h
+++ mylib.h
@@ -1,7 +1,7 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 #include <cmath>
-
+#include <cstdlib>
 
 #ifdef EXTERNC
 extern "C" {
@@ -17,5 +17,35 @@ void mandel(double *img, double re1, double re2, double im1, double im2,
 }
 #endif
 
+extern "C" double HSVolume(int d, long long N, double r)
+{
+    std::srand(42);
+
+    long long hits = 0;
+    for (long long i = 0; i < N; ++i)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < d; ++j)
+        {
+            double x = 2.0 * rand() / double(RAND_MAX) - 1.0; // uniform in [-1,1]
+            sum += x * x;
+        }
+        if (sum <= 1.0) hits++;
+    }
+
+    // cube volume = (2r)^d
+    double Vcube = std::pow(2.0 * r, d);
+    double V = (double)hits / N * Vcube;
+    return V;
+}
+
+extern "C" long long count3d_cpp(int n) {
+    long long count = 0;
+    for (int i = 0; i < n; ++i)
+        for (int j = i + 1; j < n; ++j)
+            for (int k = j + 1; k < n; ++k)
+                ++count;
+    return count;
+}
   
 #endif // block to ensure header is only compiled once
